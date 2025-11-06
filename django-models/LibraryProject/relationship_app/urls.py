45626ae49b8f074f92ef48f3d6d6a01e96891dae
Kevin Njagi <njagikl@gmail.com>
2025-11-06 15:28:04 +0300

    Include relationship_app URLs in project urlconf

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,3 +7,15 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path("register/", views.register, name="register"),
 ]
+from django.urls import path
+from .views import admin_view, librarian_view, member_view, list_books, LibraryDetailView
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # Role-based routes
+    path('roles/admin/', admin_view, name='admin_view'),
+    path('roles/librarian/', librarian_view, name='librarian_view'),
+    path('roles/member/', member_view, name='member_view'),
+]
