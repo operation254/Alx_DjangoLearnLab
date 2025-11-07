489666692907a5a35b739151e89b4e5d10dda2cc
Kevin Njagi <njagikl@gmail.com>
2025-11-07 17:15:36 +0300

    Add UserProfile (Admin/Librarian/Member) + signals for auto-create

---
+++ django-models/LibraryProject/relationship_app/signals.py
@@ -0,0 +1,22 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    UserProfile.objects.get_or_create(user=instance)
+    instance.profile.save()
+from django.apps import AppConfig
+
+class RelationshipAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'relationship_app'
+
+    def ready(self):
+        from . import signals  # noqa
