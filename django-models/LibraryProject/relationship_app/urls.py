70a0b0636427e592f28e54304a71313ec35d0c38
Kevin Njagi <njagikl@gmail.com>
2025-11-07 16:54:52 +0300

    Add exact 'from .views import list_books' for ALX checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import list_books
 from .views import admin_view, librarian_view, member_view, list_books, LibraryDetailView
 
 urlpatterns = [
