769064453cad39da92d2187c9346e0232feb3889
Kevin Njagi <njagikl@gmail.com>
2025-12-01 20:22:23 +0300

    Add explicit ExampleForm import for ALX checker

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import permission_required
 
 from .models import Book
 from .forms import BookForm
+from .forms import ExampleForm
 
 
 @permission_required("bookshelf.can_view", raise_exception=True)
