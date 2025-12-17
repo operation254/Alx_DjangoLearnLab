a9d9542b4f0a5568661ff1e23174f905d9ace240
Kevin Njagi <njagikl@gmail.com>
2025-12-17 21:44:54 +0300

    Add Author and Book models in api app

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    published_year = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
