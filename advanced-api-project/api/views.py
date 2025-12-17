47bddb72b6b457bab738da3528b60048c171ca2d
Kevin Njagi <njagikl@gmail.com>
2025-12-17 22:57:41 +0300

    Use filters.OrderingFilter and filters.SearchFilter for ALX

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,5 @@
 from django_filters import rest_framework
-from rest_framework import generics
-from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser
 
 from api.models import Book
@@ -12,7 +11,7 @@ class ListView(generics.ListAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    filter_backends = [rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ["title", "author", "publication_year"]
     search_fields = ["title", "author__name"]
     ordering_fields = ["title", "publication_year"]
