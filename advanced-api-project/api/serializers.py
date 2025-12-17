063b34993af3e2ecac98d8fe4553e4d77dd148d0
Kevin Njagi <njagikl@gmail.com>
2025-12-17 22:14:12 +0300

    Add BookSerializer validation (serializers.ValidationError)

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,3 +1,4 @@
+from datetime import date
 from rest_framework import serializers
 from api.models import Author, Book
 
@@ -12,3 +13,9 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ["id", "title", "author", "publication_year", "created_at"]
+
+    def validate_publication_year(self, value):
+        current_year = date.today().year
+        if value < 1400 or value > current_year:
+            raise serializers.ValidationError("Invalid publication_year")
+        return value
