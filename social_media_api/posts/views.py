282aa367a55f90f82bde4deccd3030397b50c5d8
Kevin Njagi <njagikl@gmail.com>
2025-12-21 22:00:35 +0300

    Fix permissions import in feed view

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics, permissions, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
