6ff9d13ef81fdfb79eaf206cbfe8b6e793669672
Kevin Njagi <njagikl@gmail.com>
2025-12-21 18:40:49 +0300

    Fix FeedView import in posts urls

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, FeedView  # <-- add FeedView
 
 router = DefaultRouter()
 router.register(r"posts", PostViewSet, basename="posts")
 router.register(r"comments", CommentViewSet, basename="comments")
 
 urlpatterns = [
+    path("feed/", FeedView.as_view(), name="feed"),
     path("", include(router.urls)),
 ]
